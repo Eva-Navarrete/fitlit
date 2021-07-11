51cc2ec0e9495e632ae6a191a1fe0b5e09344ca2
Eric Li <ericli@knights.ucf.edu>
2021-07-11 13:39:06 -0600

    Add getUserData and getAllUsersAvgSteps methods

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -2,6 +2,16 @@ class UserRepository {
   constructor(user) {
     this.data = user;
   }
+
+  getUserData(id) {
+    return this.data.find(user => user.id === id);
+  }
+
+  getAllUsersAvgSteps() {
+    return this.data.reduce((acc, user) => {
+      return acc + user.dailyStepGoal;
+    }, 0) / this.data.length;
+  }
 }
 
 export default UserRepository;
