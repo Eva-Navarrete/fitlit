4950a6434ddafda16205f7879d5d3a1cb8098820
Eric Li <ericli@knights.ucf.edu>
2021-07-11 11:31:44 -0600

    Add initial tests

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,25 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
-
+const userTestData = require('../src/data/userTestData');
 describe('User Repository', () => {
-  it('should be a function', function () {
+
+  let userRepo;
+
+  beforeEach(() => {
+    userRepo = new UserRepository(userTestData);
+  });
+
+  it('should be a function', () => {
     expect(UserRepository).to.be.a('function');
   });
+
+  it.skip('should be an instance of UserRepository', () => {
+    expect(userRepo).to.be.an.instanceof(UserRepository);
+  });
+
+  it.skip('should store all user data', () => {
+    expect(userRepo.data).to.deep.equal(userTestData);
+  });
+
+  
 });
