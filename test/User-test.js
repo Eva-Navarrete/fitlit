5bcad0251f4b9f6e75c2abe54b58d0a32f34f80d
Eva Navarrete <evanavarrete07@gmail.com>
2021-07-11 10:40:11 -0600

    add properties to user

--- test/User-test.js
+++ test/User-test.js
@@ -1,2 +1,25 @@
 import { expect } from 'chai';
-import UserRepository from '../src/User';
+import User from '../src/User';
+const userTestData = require('..src/data/activityTestData')
+
+describe('User', () => {
+  let user1
+
+  beforeEach(() => {
+    user1 = new User(userTestData[0]);
+  })
+
+  it('Should be a function', () => {
+    expect(User).to.be.('function')
+  });
+
+  it('Should be an instance of User', () => {
+    expect(user1).to.be.an.instanceof(User);
+  });
+
+  it('Should have an id', () => {
+    expect(user.id).to.equal(1);
+  });
+
+  it('Should')
+})
