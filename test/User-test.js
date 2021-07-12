753407e6c1bb23846524dd3bdf66796a0357661e
Eric Li <ericli@knights.ucf.edu>
2021-07-12 16:27:39 -0600

    Add tests for storing sleepData, hydrationData, and activityData

--- test/User-test.js
+++ test/User-test.js
@@ -1,16 +1,22 @@
 import { expect } from 'chai';
 import User from '../src/User';
 const userTestData = require('../src/data/userTestData')
+import Sleep from '../src/Sleep';
+import Hydration from '../src/Hydration';
+import Activity from '../src/Activity';
 
 describe('User', () => {
-  let user1
+  let user1, sleep, hydration, activity;
 
   beforeEach(() => {
     user1 = new User(userTestData[0]);
+    sleep = new Sleep();
+    hydration = new Hydration();
+    activity = new Activity();
   });
 
   it('Should be a function', () => {
-    expect(User).to.be.a('function')
+    expect(User).to.be.a('function');
   });
 
   it('Should be an instance of User', () => {
@@ -25,8 +31,8 @@ describe('User', () => {
     expect(user1.name).to.equal('Shay Mitchel');
   });
 
-  it('Shoud have an address', () => {
-    expect(user1.address).to.equal('809 Cherry Lane Trail, Minneapolis, MN 55125')
+  it('Should have an address', () => {
+    expect(user1.address).to.equal('809 Cherry Lane Trail, Minneapolis, MN 55125');
   });
 
   it('Should have an email', () => {
@@ -42,10 +48,31 @@ describe('User', () => {
   });
 
   it('Should have a list of friends', () => {
-    expect(user1.friends).to.deep.equal([19, 9, 31])
+    expect(user1.friends).to.deep.equal([19, 9, 31]);
   });
 
   it('Should return user first name', () => {
     expect(user1.returnName()).to.equal('Shay');
+  });
+
+  it('Should store a user\'s sleep data', () => {
+    user1.sleepData.push(sleep);
+
+    expect(user1.sleepData).to.deep.equal([sleep]);
+    expect(user1.sleepData.length).to.equal(1);
+  });
+
+  it('Should store a user\'s hydration data', () => {
+    user1.hydrationData.push(hydration);
+
+    expect(user1.hydrationData).to.deep.equal([hydration]);
+    expect(user1.hydrationData.length).to.equal(1);
+  });
+
+  it('Should store a user\'s activity data', () => {
+    user1.activityData.push(activity);
+
+    expect(user1.activityData).to.deep.equal([activity]);
+    expect(user1.activityData.length).to.equal(1);
   })
 })
