7c5d66556665f15f96c70a0593caa8e33701495b
Eva Navarrete <evanavarrete07@gmail.com>
2021-07-12 18:03:21 -0600

    Add API fetch calls files and fetch data functions

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,15 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-// An example of how you tell webpack to use a CSS file
+// An example of how you tell webpack to use a CSS
+// import { fetchCalls } from './apiCalls'
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './images/mario-run.png'
+import './images/mushroom.png'
+import './images/water-bottle.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
@@ -13,3 +17,14 @@ console.log('This is the JavaScript entry file - your code begins here.');
 
 import userData from './data/users';
 import UserRepository from './UserRepository';
+
+window.onload = generateApiCalls();
+
+
+function generateApiCalls() {
+  fetchCalls.fetchUserData()
+  .then(data => {
+    console.log(data)
+  })
+  return
+}
