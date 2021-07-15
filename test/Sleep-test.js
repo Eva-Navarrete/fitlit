6b86108b7d87921634e26d0eac2ecdb71ae73908
Eric Li <ericli@knights.ucf.edu>
2021-07-15 14:18:31 -0600

    Unskip tests for Sleep-test

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -10,27 +10,27 @@ describe('Sleep', () => {
     sleep = new Sleep(data[0]);
   });
 
-  it.skip('Should be a function', () => {
+  it('Should be a function', () => {
     expect(Sleep).to.be.a('function');
   });
 
-  it.skip('Should be an instance of Sleep', () => {
+  it('Should be an instance of Sleep', () => {
     expect(sleep).to.be.an.instanceof(Sleep);
   });
 
-  it.skip('Should hold a user\'s ID', () => {
+  it('Should hold a user\'s ID', () => {
     expect(sleep.userID).to.equal(1);
   });
 
-  it.skip('Should store a date', () => {
+  it('Should store a date', () => {
     expect(sleep.date).to.equal("2019/06/15");
   });
 
-  it.skip('Should store the number of hours a user slept', () => {
+  it('Should store the number of hours a user slept', () => {
     expect(sleep.hoursSlept).to.equal(5);
   });
 
-  it.skip('Should hold the user\'s sleep quality for the night', () => {
+  it('Should hold the user\'s sleep quality for the night', () => {
     expect(sleep.sleepQuality).to.equal(2);
   });
 });
