3ad3584c0da727817781313b18a917623eb2ebc4
Eva Navarrete <evanavarrete07@gmail.com>
2021-07-16 19:59:30 -0600

    Unskip test's

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -10,32 +10,31 @@ describe('Activity', () => {
     activity = new Activity(data[0]);
   });
 
-  it.skip('Shhould be a function', () => {
+  it('Shhould be a function', () => {
     expect(Activity).to.be.a('function');
   });
 
-  it.skip('Should be an instance of Activity', () => {
+  it('Should be an instance of Activity', () => {
     expect(activity).to.be.an.instanceof(Activity);
   });
 
-  it.skip('Should hold a user\'s ID', () => {
+  it('Should hold a user\'s ID', () => {
     expect(activity.userID).to.equal(1);
   });
 
-  it.skip('Should store a date', () => {
+  it('Should store a date', () => {
     expect(activity.date).to.equal("2019/06/15");
   });
 
-  it.skip('Should hold user\'s number of steps', () => {
+  it('Should hold user\'s number of steps', () => {
     expect(activity.numSteps).to.equal(3577);
   });
 
-  it.skip('Should hold user\'s minutes active', () => {
+  it('Should hold user\'s minutes active', () => {
     expect(activity.minutesActive).to.equal(140);
   });
 
-  it.skip('Should hold user\'s flight of stairs', () => {
+  it('Should hold user\'s flight of stairs', () => {
     expect(activity.flightsOfStairs).to.equal(16);
   });
-  
-})
+});
