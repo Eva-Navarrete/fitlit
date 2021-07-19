dd29cb48e91d196531eec851a0c88e2da3befe8c
Eva Navarrete <evanavarrete07@gmail.com>
2021-07-19 15:36:18 -0600

    Add chart canvas tag to activity section

--- src/charts.js
+++ src/charts.js
@@ -1,20 +1,58 @@
-new Chart(document.getElementById("myChart"), {
-  type: 'bar',
-  data: {
-    labels: ["2019/06/01", "2019/06/02","2019/06/03","2019/06/04","2019/06/05","2019/06/06","2019/06/07"],
-    datasets: [
-      {
-        label: "Ounces",
-        backgroundColor: ['blue','cyan', 'lime green','purple','pink', 'red', 'yellow'],
-        data: [10, 20, 30, 40, 50, 60, 70]
+// USER GOALS CHART
+
+const renderHydrationChart = (data) => {
+  return new Chart(document.getElementById("hydroChart"), {
+    type: 'bar',
+    data: {
+      labels: data.hydrationData.slice(-7).map(hydro => hydro.date),
+      datasets: [{
+        label: 'Ounces',
+        backgroundColor: ['#191970'],
+        data: data.getWeeklyOunces()
+      }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false
+        }
       }
-    ]
-  },
-  options: {
-    legend: { display: false },
-    title: {
-      display: true,
-      text: 'Weekly Hydration Log'
     }
-  }
-});
+
+  });
+}
+
+const renderActivityChart = () => {
+  return new Chart(document.getElementById("activityChart"), {
+    type: 'bar',
+    data: {
+      labels: ["2019/06/01", "2019/06/02", "2019/06/03", "2019/06/04", "2019/06/05", "2019/06/06", "2019/06/07"],
+      datasets: [{
+        label: "Ounces",
+        backgroundColor: ['blue', 'cyan', 'lime green', 'purple', 'pink', 'red', 'yellow'],
+        data: [10, 20, 30, 40, 50, 60, 70]
+      }]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Weekly Hydration Log'
+      }
+    }
+  })
+}
+
+
+// const allCharts [
+//      hydrationChart()
+// ];
+// USER GOAL VS OTHERS Chart
+
+//ACTIVITY USER STEPS/MILES/MINACTIVE PER DAY
+export {
+  renderHydrationChart,
+  renderActivityChart
+}
